37a643cd8d18f6ff31ca72a0c5ee7f118b15c12d
Veselin Todorov <veselin@veselin.bg>
2016-04-26 08:16:34 +0300

    Oops. Export the new rule

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
   rules: {
     'no-explicit-directory-imports': require('./rules/no-explicit-directory-imports'),
+    'no-relative-imports': require('./rules/no-relative-imports'),
     'spread-jsx': require('./rules/spread-jsx'),
   },
 };
